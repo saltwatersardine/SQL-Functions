bb67629b569c4d5f7cb97abb7beaa5b70dbe0fb5
saltwatersardine <109593672+saltwatersardine@users.noreply.github.com>
2023-03-28 13:34:04 +0200

    Update SQL_Cheat_Sheet.sql

--- SQL_Cheat_Sheet.sql
+++ SQL_Cheat_Sheet.sql
@@ -1,14 +1,10 @@
--- SQL Cheat Sheet
--- Github Repository: https://github.com/
-
 -- 1. Create a new table
 CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
 
 -- 2. Insert data into the table
 INSERT INTO users (username, email, password)
@@ -51,17 +47,48 @@ SELECT u.username, o.order_id
 FROM users u
 LEFT JOIN orders o ON u.id = o.user_id;
 
--- 14. Subquery
+-- 14. Right Join (RIGHT OUTER JOIN)
+SELECT u.username, o.order_id
+FROM users u
+RIGHT JOIN orders o ON u.id = o.user_id;
+
+-- 15. Subquery
 SELECT username
 FROM users
 WHERE id IN (
     SELECT user_id
     FROM orders
-    WHERE order_total > 100
-);
+    WHERE order_total > 100);
 
--- 15. Create an index
+-- 16. Create an index
 CREATE INDEX idx_users_username ON users (username);
 
--- 16. Drop an index
+-- 17. Drop an index
 DROP INDEX idx_users_username ON users;
+
+-- 18. Select distinct values from a column
+SELECT DISTINCT country FROM users;
+
+-- 19. Count the number of distinct values in a column
+SELECT COUNT(DISTINCT country) FROM users;
+
+-- 12. Limit the number of rows returned
+SELECT * FROM users LIMIT 10;
+
+-- 21. Select rows using pattern matching with LIKE
+SELECT * FROM users WHERE username LIKE 'j%';
+
+-- 22. Select rows using multiple conditions with AND
+SELECT * FROM users WHERE country = 'USA' AND age > 18;
+
+-- 23. Select rows using multiple conditions with OR
+SELECT * FROM users WHERE country = 'USA' OR country = 'Canada';
+
+-- 24. Use aggregate functions to summarize data (AVG, SUM, MIN, MAX)
+SELECT AVG(age), SUM(age), MIN(age), MAX(age) FROM users;
+
+-- 25. Group rows, apply aggregation functions, and filter using HAVING
+SELECT country, COUNT(*)
+FROM users
+GROUP BY country
+HAVING COUNT(*) > 10;
